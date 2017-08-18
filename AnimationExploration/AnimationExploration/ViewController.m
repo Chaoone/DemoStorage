847ceb5b9ad398e6aad158f56b9a582c95b8772c
gghgh <chaoone163@gmail.com>
2017-08-18 14:12:46 +0800

    写了一个小动画

--- AnimationExploration/AnimationExploration/ViewController.m
+++ AnimationExploration/AnimationExploration/ViewController.m
@@ -16,13 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    animation.fromValue = [NSNumber numberWithFloat:1.f];
+    animation.toValue = [NSNumber numberWithFloat:0.f];
+    animation.autoreverses = YES; // 回退动画;
+    animation.duration = 1.f;
+    animation.repeatCount = MAXFLOAT;
+    animation.removedOnCompletion = NO;
+    animation.fillMode = kCAFillModeForwards; // removedOnCompletion, fillMode配合使用保持动画完成效果;
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    
+    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(200, 200, 64, 64)];
+    imgView.backgroundColor = [UIColor clearColor];
+    imgView.image = [UIImage imageNamed:@"heart"];
+    [self.view addSubview:imgView];
+    [imgView.layer addAnimation:animation forKey:@"aAlpha"];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
