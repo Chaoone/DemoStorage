27ba1a8e66146f50afc3f4af4bf172338b652732
gghgh <chaoone163@gmail.com>
2017-08-18 11:43:43 +0800

    核心动画的基本探究

---
+++ AnimationExploration/AnimationExploration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationExploration
+//
+//  Created by Harden JC on 2017/8/18.
+//  Copyright © 2017年 chaoone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationExploration/AnimationExploration/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimationExploration
+//
+//  Created by Harden JC on 2017/8/18.
+//  Copyright © 2017年 chaoone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
