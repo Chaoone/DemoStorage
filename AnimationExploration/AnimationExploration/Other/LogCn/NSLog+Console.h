e49f4bcf5875c5c5f5462cb9cdaabfd7bde66397
gghgh <chaoone163@gmail.com>
2017-10-17 17:34:41 +0800

    no message

---
+++ AnimationExploration/AnimationExploration/Other/LogCn/NSLog+Console.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+/** 数组NSLog打印扩展*/
+@interface NSArray (Console)
+
+@end
+
+/** 字典NSLog打印扩展*/
+@interface NSDictionary (Console)
+
+@end
+
+/** NSSet打印扩展*/
+@interface NSSet (Console)
+
+@end
